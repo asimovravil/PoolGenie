d22e89c49a7de8dff9625b1d88c03be3c079f0a8
Ravil <asimovravil06@gmail.com>
2024-03-21 15:37:38 +0500

    [PG] AppDelegate - structure

--- PoolGenie/AppDelegate.swift
+++ PoolGenie/AppDelegate.swift
@@ -9,5 +9,15 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-}
+    
+    var window: UIWindow?
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupAppearance()
+        return true
+    }
+    
+    func setupAppearance() {
+        print("setupAppearance")
+    }
+}
